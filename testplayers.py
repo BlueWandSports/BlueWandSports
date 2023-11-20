270b319484b9c7a63cee6cbc8cdbfdcfc0ecb60e
BlueWandSports <bluewandsports@gmail.com>
2023-11-20 13:19:55 -0600

    change script to output to txt for example usage

--- testplayers.py
+++ testplayers.py
@@ -2,16 +2,22 @@ import pandas as pd
 import re
 from nba_api.stats.library.data import players
 from nba_api.stats.static import players
-import time
+
+# path to text file
+path = '/home/ubuntu/BlueWandSports/active_players.txt'
 
 # get_players returns a list of dictionaries, each representing a player.
 nba_players = players.get_players()
 print('Number of players fetched: {}'.format(len(nba_players)))
-nba_players[:5]
+#nba_players[:5]
 
 list = pd.DataFrame(nba_players, columns = ['id', 'full_name', 'first_name', 'last_name', 'is_active'])
 
 # set only active
 print("Existing Dataframe is :\n", (list.loc[list.is_active]))
 
-time.sleep(15)
+# use this to write results to file
+with open(path, 'a') as f:
+    list_string = list.to_string(header=False, index=False)
+    f.write(list_string)
+
